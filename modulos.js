3e39531528bb750ac94ca3242574acc955c3c557
Guilherme R. Woelke <guilherme_wolke@yahoo.com>
2016-02-06 13:40:15 -0200

    Módulos

---
+++ modulos.js
@@ -0,0 +1,6 @@
+var modulo = require('./modulos/mod1')
+    ,pessoa = require("./modulos/mod2");
+   
+modulo("Guilherme");
+pessoa.minhaIdade(32);
+pessoa.minhaProfissao("Analista de Sistemas Realizado!");
---
+++ modulos/mod2.js
@@ -0,0 +1,7 @@
+exports.minhaIdade = function(idade){
+    console.log("Minha Idade: " + idade);
+}
+
+exports.minhaProfissao = function(profissao){
+    console.log("Minha Profissão: " + profissao);
+}
